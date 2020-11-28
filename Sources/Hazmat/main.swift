28976f0e9a491258bfae34c599c815331e39b331
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-11-28 13:57:11 -0600

    add basic (albeit useless) functionality

--- Sources/Hazmat/main.swift
+++ Sources/Hazmat/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct Hazmat: ParsableCommand {
+    @Argument var name: String
+    @Argument var path: String
+    
+    mutating func run() throws {
+        print("Checking '\(name)' in '\(path)'")
+    }
+}
+
+Hazmat.main()
